b354f8e93ff92a01527c197775f0e887ef4dbf4c
Walter <relw1wa-github@yahoo.de>
2016-04-28 15:31:32 +0200

    Add comments to school attendants original application

--- Javascript-Design-Patterns/School-Attendance/js/app.js
+++ Javascript-Design-Patterns/School-Attendance/js/app.js
@@ -3,6 +3,9 @@
  * attendance record if one is not found
  * within localStorage.
  */
+/* Comment:
+   this IIFE created random data set for all the students and stores
+   it in localStorage if there's no data in localStorage yet */
 (function() {
     if (!localStorage.attendance) {
         console.log('Creating attendance records...');
@@ -29,11 +32,20 @@
 
 /* STUDENT APPLICATION */
 $(function() {
+		/* Comment:
+		 	- get attendance data from localStorage (= model)
+			- also setup references to .missed-col and all inputs (= view) */
     var attendance = JSON.parse(localStorage.attendance),
         $allMissed = $('tbody .missed-col'),
         $allCheckboxes = $('tbody input');
 
     // Count a student's missed days
+		/* Comment:
+		 	- function that counts the days each student missed
+			- the sum of missed days is stored in the view and is updated via this function
+			- it's not possible to just update one student's missed days
+			- it gets called for the first time after loading is complete and after the each
+				loop below */
     function countMissing() {
         $allMissed.each(function() {
             var studentRow = $(this).parent('tr'),
@@ -45,12 +57,17 @@ $(function() {
                     numMissed++;
                 }
             });
-
             $(this).text(numMissed);
         });
     }
 
     // Check boxes, based on attendace records
+		/* Comment:
+			- this is the first function to run
+			- each loop runs over attendance object
+			- for each student found, the respective table row is found
+			- the inputs of this row are referenced
+			- another each loop sets attendance in each input according to true/false in days[i] */
     $.each(attendance, function(name, days) {
         var studentRow = $('tbody .name-col:contains("' + name + '")').parent('tr'),
             dayChecks = $(studentRow).children('.attend-col').children('input');
@@ -61,6 +78,11 @@ $(function() {
     });
 
     // When a checkbox is clicked, update localStorage
+	 	/* Comment:
+			- event listener that is called after changing attendance for one student
+			- data for all students gets updated, not possible to just update one student's data
+			- countMissing is called to update sums of days missed
+			- data in localStorage gets updated */
     $allCheckboxes.on('click', function() {
         var studentRows = $('tbody .student'),
             newAttendance = {};
