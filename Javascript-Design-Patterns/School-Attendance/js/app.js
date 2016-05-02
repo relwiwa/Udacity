ca5692ef4892671f160c54e7375efc3d475238f5
Walter <relw1wa-github@yahoo.de>
2016-05-02 09:25:21 +0200

    Update school attendance app, now individual ID is unique key

--- Javascript-Design-Patterns/School-Attendance/js/app.js
+++ Javascript-Design-Patterns/School-Attendance/js/app.js
@@ -6,29 +6,41 @@
    application following the "burn it"-approach
  - It also uses localStorage, where all model data is
  	 stored and kept up to date
- - student's names are considered unique in this version
+ - model data is now organized upon an unique student id,
+ 	 and not the student's name
  ********************************************************/
 
 var model = {
-	names: ["Slappy the Frog", "Lilly the Lizard", "Paulrus the Walrus", "Gregory the Goat", "Adam the Anaconda"],	
+	names: ["Slappy the Frog", "Lilly the Lizard", "Paulrus the Walrus", "Gregory the Goat", "Adam the Anaconda"],
+	idCounter: 0,
 
 	/* init function
 			- sets up data and attendance randomly if there's no data in localStorage */
 	init: function() {
 		if (!localStorage.students) {
-			var students = {};
-			for (var i = 0; i < names.length; i++) {
-				students[model.names[i]] = {
-					attendance: model.createAttendance(),
-					id: i
-				};
+			localStorage.students = JSON.stringify({});
+			for (var i = 0; i < model.names.length; i++) {
+				model.addStudent(model.names[i]);
 			}
-			/* stringify is necessary, as localStorage only stores simple one-dimensional key-value pairs,
-			   and no complex javascript object structures */
-			localStorage.students = JSON.stringify(students);
 		}
 	},
 	
+	/* addStudent:
+			- adds a student to localStorage.students
+			- adds a unique id and increases idCounter */
+	addStudent: function(name) {
+		var newStudent = {
+			"name": name,
+			"attendance": model.createAttendance()			
+		}
+		var students = octopus.getAllStudents();
+		students[model.idCounter] = newStudent;
+		/* stringify is necessary, as localStorage only stores simple one-dimensional key-value pairs,
+		   and no complex javascript object structures */
+		localStorage.students = JSON.stringify(students);
+		model.idCounter++;
+	},
+		
 	/* createAttendance function:
 			- returns an array with twelve slots and random true/false values */
 	createAttendance: function() {
@@ -41,9 +53,9 @@ var model = {
 	
 	/* updateAttendance function:
 			- attendance of one student gets updated in localStorage */
-	updateAttendance: function(name, newAttendance) {
+	updateAttendance: function(id, newAttendance) {
 		var attd = JSON.parse(localStorage.students);
-		attd[name].attendance = newAttendance;
+		attd[id].attendance = newAttendance;
 		localStorage.students = JSON.stringify(attd);
 	}
 
@@ -52,8 +64,8 @@ var model = {
 
 var octopus = {
 	
-	getStudent: function(name) {
-		var student = JSON.parse(localStorage.students)[name];
+	getStudent: function(id) {
+		var student = JSON.parse(localStorage.students)[id];
 		if (student) {
 			return student;
 		}
@@ -62,25 +74,25 @@ var octopus = {
 		}
 	},
 	
-	getAllStudents: function(name) {
+	getAllStudents: function() {
 		return JSON.parse(localStorage.students);
 	},
 	
 	/* updateAttendance function:
 			- updates localStorage by toggling the stored value via name and position
 			- updates sum of missed days via student.id by increasing or decreasing current value by 1 */
-	updateAttendance: function(name, position) {
-		var student = octopus.getStudent(name);
+	updateAttendance: function(id, position) {
+		var student = octopus.getStudent(id);
 		var attd = student.attendance;
 		if (attd[position] === true) {
 			attd[position] = false;
-			view.updateSum(student.id, 1)
+			view.updateSum(id, 1)
 		}
 		else {
 			attd[position] = true;
-			view.updateSum(student.id, -1);
+			view.updateSum(id, -1);
 		}
-		model.updateAttendance(name, attd);
+		model.updateAttendance(id, attd);
 	},
 	
 	init: function() {
@@ -105,7 +117,7 @@ var view = {
 			var tr = table.insertRow(-1);
 			tr.setAttribute("class", "student");
 			var tdName = tr.insertCell(-1);
-			tdName.innerHTML = key;
+			tdName.innerHTML = students[key].name;
 			tdName.setAttribute("class", "name-col");
 			for (var i = 0; i < currAttd.length; i++) {
 				var tdInput = tr.insertCell(-1);
@@ -126,7 +138,7 @@ var view = {
 			}
 			var tdSum = tr.insertCell(-1);
 			tdSum.setAttribute("class", "missed-col");
-			tdSum.setAttribute("id", "sum-" + students[key].id);
+			tdSum.setAttribute("id", "sum-" + key);
 			tdSum.textContent = 12 - sum;
 		}
 	},
