6bb3d0f60f4160b6f0548df80f7c6e87b805f554
Walter <relw1wa-github@yahoo.de>
2016-05-12 09:22:33 +0200

    Add solution to Angular Quiz 'Bootstrap' in Frontend Frameworks course

---
+++ Frontend-Frameworks/Quiz-Angular-Bootstrap/app/scripts/legoBuilder.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name newModuleQuizApp
+ * @description
+ * # legoBuilder
+ *
+ * Main module of the application.
+ */
+angular
+  .module('legoBuilder', []);
