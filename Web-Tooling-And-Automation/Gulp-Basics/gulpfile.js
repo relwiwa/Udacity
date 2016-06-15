94e4e3f77bf35176433b05cd3f7459286772a401
Walter <relw1wa-github@yahoo.de>
2016-06-15 13:56:26 +0200

    Web Tooling: Add gulp autoprefixer module and add it to styles task

--- Web-Tooling-And-Automation/Gulp-Basics/gulpfile.js
+++ Web-Tooling-And-Automation/Gulp-Basics/gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
 	console.log("Hello World");
@@ -11,6 +12,10 @@ gulp.task('styles', function() {
 	gulp.src('sass/**/*.scss')
 		// run sass() and handle errors in order to prevent build abort
 		.pipe(sass().on('error', sass.logError))
+		// run autoprefixer
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions', '> 1%', 'ie > 8']
+		}))
 		// state output directory for css files
 		.pipe(gulp.dest('./css'))
 });
