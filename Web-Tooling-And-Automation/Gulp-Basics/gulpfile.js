c00e3f7bf605873dfa0793a20098c6da437cd9d7
Walter <relw1wa-github@yahoo.de>
2016-06-15 13:32:06 +0200

    Web Tooling: Add gulp sass module and create styles task

--- Web-Tooling-And-Automation/Gulp-Basics/gulpfile.js
+++ Web-Tooling-And-Automation/Gulp-Basics/gulpfile.js
@@ -1,5 +1,16 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('default', function() {
 	console.log("Hello World");
+});
+
+// styles task to tranform scss to css files
+gulp.task('styles', function() {
+	// state input directory with scss files
+	gulp.src('sass/**/*.scss')
+		// run sass() and handle errors in order to prevent build abort
+		.pipe(sass().on('error', sass.logError))
+		// state output directory for css files
+		.pipe(gulp.dest('./css'))
 });
