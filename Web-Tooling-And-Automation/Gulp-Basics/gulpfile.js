a3cdb2af8fd317c9235b98ad147c6bc5e80c7d97
Walter <relw1wa-github@yahoo.de>
2016-06-15 14:27:39 +0200

    Web Tooling: Add gulp eslint module and create eslint task

--- Web-Tooling-And-Automation/Gulp-Basics/gulpfile.js
+++ Web-Tooling-And-Automation/Gulp-Basics/gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var eslint = require('gulp-eslint');
 
-gulp.task('default', function() {
+gulp.task('default', ['eslint'], function() {
 	console.log("Hello World");
 	gulp.watch('sass/**/*.scss', ['styles']);
+	gulp.watch('js/**/*js', ['eslint']);
+});
+
+// code snippet taken from: https://www.npmjs.com/package/gulp-eslint
+gulp.task('eslint', function () {
+	// ESLint ignores files with "node_modules" paths.
+	// So, it's best to have gulp ignore the directory as well.
+	// Also, Be sure to return the stream from the task;
+	// Otherwise, the task may end before the stream has finished.
+	return gulp.src(['js/**/*.js', '!node_modules/**'])
+	// eslint() attaches the lint output to the "eslint" property
+	// of the file object so it can be used by other modules.
+		.pipe(eslint())
+		// eslint.format() outputs the lint results to the console.
+		// Alternatively use eslint.formatEach() (see Docs).
+		.pipe(eslint.format())
+		// To have the process exit with an error code (1) on
+		// lint error, return the stream and pipe to failAfterError last.
+		.pipe(eslint.failAfterError());
 });
 
 // styles task to tranform scss to css files
