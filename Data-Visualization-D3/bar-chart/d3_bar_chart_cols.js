8a193a8b76c66295fa82f3f63043e1486c7f147e
Walter <relw1wa-github@yahoo.de>
2016-12-05 15:00:05 +0100

    Data Vis + D3: Bar Chart III- Add x- and y-axis to chart

--- Data-Visualization-D3/bar-chart/d3_bar_chart_cols.js
+++ Data-Visualization-D3/bar-chart/d3_bar_chart_cols.js
@@ -1,15 +1,35 @@
-var width = 960;
-var height = 500;
+var margin = {
+  top: 20,
+  right: 30,
+  bottom: 30,
+  left: 40
+};
+var width = 960 - margin.left - margin.right;
+var height = 500 - margin.top - margin.bottom;
 
 var x = d3.scale.ordinal()
   .rangeRoundBands([0, width], .1);
 
+var xAxis = d3.svg.axis()
+  .scale(x)
+  .orient("bottom");
+
 var y = d3.scale.linear()
   .range([height, 0]);
 
+var yAxis = d3.svg.axis()
+  .scale(y)
+  .orient("left");
+
+/*  "To apply the margins to the SVG container, we set the width
+    and height of the SVG element to the outer dimensions, and add
+    a g element to offset the origin of the chart area by the
+    top-left margin." */
 var chart = d3.select(".chart")
-  .attr("width", width)
-  .attr("height", height);
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 d3.tsv("letters.tsv", toInteger, function(error, data) {
   x.domain(data.map(function(d) {
@@ -19,16 +39,22 @@ d3.tsv("letters.tsv", toInteger, function(error, data) {
     return d.frequency;
   })]);
   
-  var barWidth = width/data.length;
+  chart.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
   
-  var bar = chart.selectAll("g")
+  chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis);
+
+  chart.selectAll(".bar")
     .data(data)
-  .enter().append("g")
-    .attr("transform", function(d) {
-      return "translate(" + x(d.letter) + ",0)";
-    });
-  
-  bar.append("rect")
+  .enter().append("rect")
+    .attr("class", "bar")
+    .attr("x", function(d) {
+      return x(d.letter);
+    })
     .attr("y", function(d) {
       return y(d.frequency);
     })
